ebaf718fe7b97e31340b0cec84357e6337363936
diegoruro <diegofernandoruizrojas@gmail.com>
2022-10-28 12:19:39 -0500

    back

---
+++ back/sparkmonolito/src/main/java/edu/eci/arep/SparkWebApp.java
@@ -0,0 +1,22 @@
+package edu.eci.arep;
+
+import static spark.Spark.*;
+
+public class SparkWebApp {
+    public static void main(String... args){
+        port(getPort());
+        staticFiles.location("/public");
+        post("message", (req,res) -> {
+            String response=null;
+            System.out.println("posteando");
+            System.out.println(req.body());
+            return response;});
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
