23b3c2ccd860280b18e7d0e22c891b15f0af05f9
Juan Rojas <juan.rojas-castr@mail.escuelaing.edu.co>
2022-11-14 12:42:27 -0500

    maquetación finalizada

--- front/src/main/resources/public/app.js
+++ front/src/main/resources/public/app.js
@@ -1,15 +1,26 @@
-function loadGetMsg() {
+function login() {
+    // JWT then ->
+
     let header = document.getElementById('header');
     header.setAttribute('isloggin', 'true');
 
     let body = document.getElementById('body');
     body.setAttribute('currentview', 'home');
+}
 
-    // let message = document.getElementById("message").value;
-    // const xhttp = new XMLHttpRequest();
-    // xhttp.onload = function () {
-    // }
-    // xhttp.open("POST", "/message");
-    // xhttp.setRequestHeader("content-type", "text/plain")
-    // xhttp.send(message);
+function sendTweet() {
+    let tweet = document.getElementById('tweet').value;
+    let stream = document.getElementById('stream');
+
+    let tweetTemplate = `<article class="tweet">
+    <img class="prof-img" width="50px" height="50px" src="https://pbs.twimg.com/profile_images/1509033228874694659/KjCCiVZI_400x400.jpg" alt="">
+    <div class="tweet-content">
+        <div class="tweet-user">
+            <h3 class="tweet-name">Juan Rojas</h3><p class="tweet-nickname">@juanro</p>
+        </div>
+        <p class="tweet-msg">${tweet}</p>
+    </div>
+    </article>`;
+    stream.innerHTML += tweetTemplate;
+    document.getElementById('tweet').value = '';
 }
